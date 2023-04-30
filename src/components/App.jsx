9ff99c438a6827edf79f5fac48c4282b9198fbb0
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-04-30 20:35:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,31 @@ import { Component } from "react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from "./Searchbar/Searchbar";
-
-
+import ImageGallery from "./ImageGallery/ImageGallery";
 export class App extends Component {
-   state = {
-    searchQuery: '',
+  state = {
+    images: [],
+    query: '',
+    page: 1,
   };
 
-    componentDidUpdate(prevProps, prevState) {
+   componentDidUpdate(prevProps, prevState) {
   }
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery })
-    console.log(searchQuery);
-  }
-  
+  handleSubmit = query => {
+    this.setState({ query: query, images: [], page: 1 });
+  };
+
   render() {
+    const { images} = this.state;
 
     return (
       <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={this.handleSubmit} />
+        <ImageGallery images={images} />
         <ToastContainer autoClose={3000} />
       </div>
-    )
-
+    );
   }
-};
+}
+
